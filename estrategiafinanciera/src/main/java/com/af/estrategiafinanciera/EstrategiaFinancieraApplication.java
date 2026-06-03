f6928fabbca5cbaa66a845fb0caf7568f824902e
Angel-D28 <angeld.diazp@uqvirtual.edu.co>
2026-06-02 22:13:17 -0500

    First Commit, creation and structuration of the project

---
+++ estrategiafinanciera/src/main/java/com/af/estrategiafinanciera/EstrategiaFinancieraApplication.java
@@ -0,0 +1,13 @@
+package com.af.estrategiafinanciera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstrategiaFinancieraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstrategiaFinancieraApplication.class, args);
+	}
+
+}
---
+++ estrategiafinanciera/src/test/java/com/af/estrategia_financiera/EstrategiaFinancieraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.af.estrategia_financiera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstrategiaFinancieraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
